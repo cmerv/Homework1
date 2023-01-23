fb7a484a181649ca486ece6279c59e256d7670bd
Christopher Stephens <cstep98@outlook.com>
2023-01-22 19:09:58 -0600

    New program made!

---
+++ CIDM2315-Homework1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CIDM2315_Homework1 { 
+using System;
+
+class MathProgram
+{
+    static void Main(string[] args)
+    {
+        double X = 2.5;
+        double Y = 3.3;
+        double Z = 4*X*X + 3*Y;
+        Console.WriteLine(Z);
+    }
+}
+}
