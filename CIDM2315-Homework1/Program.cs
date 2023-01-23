f49ec85fc4d0170f2a422313b62355b861eb91c4
Christopher Stephens <cstep98@outlook.com>
2023-01-22 19:20:34 -0600

    More commentary and output

--- CIDM2315-Homework1/Program.cs
+++ CIDM2315-Homework1/Program.cs
@@ -1,6 +1,8 @@
 ﻿namespace CIDM2315_Homework1 { 
 using System;
-
+/* Christopher Stephens
+CIDM 2315
+*/
 class MathProgram
 {
     static void Main(string[] args)
@@ -11,7 +13,10 @@ class MathProgram
         double Y = 3.3;
         // defines float Z as 4 times X^2 added to 3 times Y
         double Z = 4*X*X + 3*Y;
-        Console.WriteLine(Z);
+        string outputXY = "X = " + X + ", Y = " + Y;
+        string outputZ = "The value of Z is " + Z + ",";
+        Console.WriteLine(outputXY);
+        Console.WriteLine(outputZ);
     }
 }
 }
