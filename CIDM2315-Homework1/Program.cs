43a2d62c036afe4e72b299b5466a6bfb66dedc27
Christopher Stephens <cstep98@outlook.com>
2023-01-22 19:14:29 -0600

    Added commentary

--- CIDM2315-Homework1/Program.cs
+++ CIDM2315-Homework1/Program.cs
@@ -5,8 +5,11 @@ class MathProgram
 {
     static void Main(string[] args)
     {
-        double X = 2.5;
+        // defines float X as 2.5
+        double X = 2.5; 
+        // defines float Y as 3.3
         double Y = 3.3;
+        // defines float Z as 4 times X^2 added to 3 times Y
         double Z = 4*X*X + 3*Y;
         Console.WriteLine(Z);
     }
